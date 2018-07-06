279885a7d04d20be241f92489153f484fa303795
nataliamh.88@gmail.com <nataliamh.88@gmail.com>
2018-07-06 17:01:44 -0400

    Transicion a otra pagina

--- src/js/app.js
+++ src/js/app.js
@@ -6,16 +6,15 @@ $(document).ready(function(){
 let valEmail = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$';
 let valPass = '[0-9]{8}';
 
-saveUserPassword = () => { //funcion para guardar comentario
-  //validación inputs, usamos if para que no se imprima un mensaje vacío
+saveUserPassword = () => { 
   document.getElementById('buttonValidate').addEventListener('click', redirect = () => {
   if((document.getElementById('identity').value === '') || (document.getElementById('password').value === '')) {
     alert('no puedes dejar campos vacíos') 
   } else {
     let email = document.getElementById('identity').value;
     let password = document.getElementById('password').value;
-    localStorage.setItem(email, password); //En este punto guardo nombre y comment en mi local storage 
-    window.location="file:///home/laboratoria/Documents/Laboratoria/Proyectos/TransLab/index3ObSaldo.html";
+    localStorage.setItem(email, password); // guardar datos en localStorage
+    window.location="file:///home/laboratoria/Documents/Laboratoria/Proyectos/TransLab/src/index2.html";
     } 
   })
 }
