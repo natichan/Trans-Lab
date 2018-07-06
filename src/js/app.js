55b95a0ed537b9488f9172c7814c8980d44257ef
nataliamh.88@gmail.com <nataliamh.88@gmail.com>
2018-07-06 15:10:02 -0400

    Modificación y orden por carpetas

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,20 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
-function saveUserPassword() { //funcion para guardar comentario
+let valEmail = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$';
+let valPass = '[0-9]{8}';
+
+saveUserPassword = () => { //funcion para guardar comentario
   //validación inputs, usamos if para que no se imprima un mensaje vacío
-  document.getElementById('buttonValidate').addEventListener('click', function Redirect() {
+  document.getElementById('buttonValidate').addEventListener('click', redirect = () => {
   if((document.getElementById('identity').value === '') || (document.getElementById('password').value === '')) {
     alert('no puedes dejar campos vacíos') 
   } else {
-    (document.getElementById('identity').value === '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') || (document.getElementById('password').value === '[0-9]{8}')
     let email = document.getElementById('identity').value;
     let password = document.getElementById('password').value;
     localStorage.setItem(email, password); //En este punto guardo nombre y comment en mi local storage 
     window.location="file:///home/laboratoria/Documents/Laboratoria/Proyectos/TransLab/index3ObSaldo.html";
-    }
+    } 
   })
 }
 saveUserPassword();
